b66282d5c6cbd5523d457cb8bd4f6e02e5f87d30
Douglas Hellinger <doug.hellinger@hotmail.com>
2018-03-31 11:06:13 +0800

    declare module param `who` with default value "anonymous"

--- hello-1.c
+++ hello-1.c
@@ -3,9 +3,12 @@
 #include <linux/init.h>
 MODULE_LICENSE("GPL");
 
+static char* who = "anonymous";
+module_param(who, charp, 0000);
+
 static int hello_init(void)
 {
-  printk(KERN_ALERT "Hello, world\n");
+  printk(KERN_ALERT "Hello, %s\n", who);
   return 0;
 }
 static void hello_exit(void)
